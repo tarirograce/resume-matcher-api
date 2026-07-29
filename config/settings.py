3e30848e6b23041c91c8d6448de3816c16f5896f
Tariro Shumba <tarirograceshumba2@gmail.com>
2026-07-29 18:21:47 -0500

    Add JWT authentication with djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
